d9be851e8f8b14746a76c588b43a9adab1e6050a
vision_chan <vision_chan@hotmail.com>
2016-12-01 13:10:09 +0800

    这是我写的第二个项目

---
+++ text/text/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  text
+//
+//  Created by Mac on 2016/12/1.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int num=10;
+        int num1;
+        // insert code here...
+        scanf("%i",&num);
+        scanf("%i",&num1);
+        NSLog(@"The number is: %i , %i",num,num1);
+        
+    }
+    return 0;
+}
